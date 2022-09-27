93909cf6f038af6f4dd974e9d988008a2474cf3c
WontCompute <86885873+WontCompute@users.noreply.github.com>
2022-09-27 07:16:58 +0200

    commits

---
+++ portfolio/src/components/header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class header extends React.Component{
+    render(){
+        return(
+            <div id="header">
+                <img></img>
+                <h1 id="name"></h1>
+            </div>
+        )
+    }
+}
